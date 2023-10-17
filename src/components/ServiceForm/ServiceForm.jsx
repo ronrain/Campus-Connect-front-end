959b30d8d40ecce98b92921bceea3029642727f2
RonRain <rarainero@gmail.com>
2023-10-16 20:49:54 -0400

    update serviceForm and indentation

--- src/components/ServiceForm/ServiceForm.jsx
+++ src/components/ServiceForm/ServiceForm.jsx
@@ -27,79 +27,79 @@ const NewService = (props) => {
   return (
     <main>
       <Sidebar />
-        <div className={styles.container}>
-      <form className={styles.serviceForm}>
-      <div className={styles.inputContainer}>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          value={formData.title}
-          onChange={handleChange}
-          placeholder=" "
-        />
-        <label htmlFor="title" className={styles.label}>Title</label>
-      </div>
+      <div className={styles.container}>
+        <form className={styles.serviceForm}>
         <div className={styles.inputContainer}>
-        <input
-          required
-          type="text"
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
-          placeholder=" "
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={formData.title}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <label htmlFor="title" className={styles.label}>Title</label>
+        </div>
+        <div className={styles.inputContainer}>
+          <input
+            required
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            placeholder=" "
           />
           <label className={styles.label}htmlFor="description">Description</label>
         </div>
         <div className={styles.inputContainer}>
-        {/* <label className={styles.label} htmlFor="type">Type</label> */}
-        <select
-          name="type"
-          value={formData.type}
-          onChange={handleChange}
+          {/* <label className={styles.label}   htmlFor="type">Type</label> */}
+          <select
+            name="type"
+            value={formData.type}
+            onChange={handleChange}
           >
-          <option value="">Select Type</option>
-          <option value="Haircut">Haircut</option>
-          <option value="Tutoring">Tutoring</option>
-          <option value="HandyWork">HandyWork</option>
-          <option value="Cooking">Cooking</option>
-          <option value="DIY">DIY</option>
-          <option value="Other">Other</option>
-        </select>
+            <option value="">Select Type</option>
+            <option value="Haircut">Haircut</option>
+            <option value="Tutoring">Tutoring</option>
+            <option value="HandyWork">HandyWork</option>
+            <option value="Cooking">Cooking</option>
+            <option value="DIY">DIY</option>
+            <option value="Other">Other</option>
+          </select>
         </div>
         <div className={styles.inputContainer}>
-        <label htmlFor="price">Price</label>
-        <input
-          required
-          type="number"
-          min={0}
-          name="price"
-          value={formData.price}
-          onChange={handleChange}
+          <label className={styles.price} htmlFor="price">Price</label>
+          <input
+            required
+            type="number"
+            min={0}
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
           />
-          </div>
-          <div className={styles.inputContainer}>
-        <input
-          required
-          type="text"
-          name="availability"
-          value={formData.availability}
-          onChange={handleChange}
-          placeholder=" "
+        </div>
+        <div className={styles.inputContainer}>
+          <input
+            required
+            type="text"
+            name="availability"
+            value={formData.availability}
+            onChange={handleChange}
+            placeholder=" "
           />
           <label className={styles.label} htmlFor="availability">Availability</label>
-          </div>
-          <div className={styles.inputContainer}>
-        <select name="schoolId" value={formData.school} onChange={handleChange}>
-          <option value="">Select School</option>
-          {props.schools.map(school => (
-            <option key={school._id} value={school._id}>{school.name}</option>
-            ))}
-        </select>
         </div>
-        <button className={styles.button30} type="submit" onClick={handleSubmit}>Submit</button>
-      </form>
-            </div>
+        <div className={styles.inputContainer}>
+          <select name="schoolId" value={formData.school} onChange={handleChange}>
+            <option value="">Select School</option>
+            {props.schools.map(school => (
+              <option key={school._id} value={school._id}>{school.name}</option>
+            ))}
+          </select>
+        </div>
+          <button className={styles.button30} type="submit"   onClick={handleSubmit}>Submit</button>
+        </form>
+      </div>
     </main>
   );
 }
