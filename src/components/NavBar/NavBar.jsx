0fced4da2cfc72dfeca9a2f476fd0a2cd4f96d89
MrXmit <tonixsmit@gmail.com>
2023-10-18 13:14:34 +0300

    make Logo a link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = ({ user, handleLogout }) => {
     <nav className={styles.container}>
       {user ?
         <ul>
-          <img src={logo} className={styles.logo}/>
+          <NavLink to="/"><img src={logo} alt="CampusConnect" className={styles.logo}/></NavLink>
           <li>Welcome, {user.name}</li>
           <li><NavLink to="/schools">Schools</NavLink></li>
           <li><NavLink to="/profile">Profiles</NavLink></li>
